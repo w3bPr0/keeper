8e7a9e271c70d5f3dcb5c6a581dd53ee6899f30e
w3bpr0 <bojd3v@gmail.com>
2023-02-09 14:40:33 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(note=><Note title={note.title} content={note.content} />)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
